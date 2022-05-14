cb81ab71371488f1d8d195b077794e569060f5a1
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-14 22:59:55 +0530

    changes-2022-05-14-22-59-55

---
+++ src/test/java/step_defs/GoogleSearchSteps.java
@@ -0,0 +1,20 @@
+package step_defs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSearchSteps {
+
+    @Given("Open Google Home Page")
+    public void openGoogleHomePage() {
+    }
+
+    @When("Text {string} is entered")
+    public void textIsEntered(String arg0) {
+    }
+
+    @Then("results for {string} is shown")
+    public void resultsForIsShown(String arg0) {
+    }
+}
