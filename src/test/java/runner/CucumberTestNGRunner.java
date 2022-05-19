f6ae459f9186a702e69eec65a1427f8ade3ee625
Gaurav Khanna <iamgauravkhanna@gmail.com>
2022-05-19 22:54:26 +0530

    changes-2022-05-19-22-54-26

--- src/test/java/runner/CucumberTestNGRunner.java
+++ src/test/java/runner/CucumberTestNGRunner.java
@@ -2,15 +2,15 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"step_definitions"},
-        plugin = {"pretty", "html:target/cucumber.html"}
+        plugin = {"pretty", "html:target/cucumber.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/html/ExtentReport123.html"},
+        tags = "@cart"
 )
-public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+public class CucumberTestNGRunner extends AbstractTestNGCucumberTests {
 
     @Override
     @DataProvider(parallel = true)
